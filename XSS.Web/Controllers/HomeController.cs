a549bdc29bc56cbf9dcc8420d7b2fbdb5307316b
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-09-22 13:42:56 +0300

    reflected-stored

---
+++ XSS.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using XSS.Web.Models;
+
+namespace XSS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CommentAdd()
+        {
+            HttpContext.Response.Cookies.Append("email", "batuhan.bedir@hotmail.com");
+            HttpContext.Response.Cookies.Append("password", "123");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CommentAdd(string name, string comment)
+        {
+            ViewBag.name = name;
+            ViewBag.comment = comment;
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
